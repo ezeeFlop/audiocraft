ac1e57551313d3abc3c437c10e1e3ddd59408876
CVE <verdier1706@gmail.com>
2024-01-22 14:42:40 +0100

    Add partial support for autocast to run on MPS (WIP)

--- audiocraft/utils/autocast.py
+++ audiocraft/utils/autocast.py
@@ -5,8 +5,35 @@
 # LICENSE file in the root directory of this source tree.
 
 import torch
+from torch.types import _dtype
+from typing import Optional, Any
+import functools
+
+def autocast_decorator(autocast_instance, func):
+  @functools.wraps(func)
+  def decorate_autocast(*args, **kwargs):
+    with autocast_instance:
+      return func(*args, **kwargs)
+  decorate_autocast.__script_unsupported = '@autocast() decorator is not supported in script mode'
+  return decorate_autocast
 
 
+class totally_legit_autocast:
+  def __init__(
+      self,
+      device_type: str,
+      dtype: Optional[_dtype] = None,
+      enabled: bool = True,
+      cache_enabled: Optional[bool] = None,
+  ): pass
+  def __enter__(self): pass
+  def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any): pass
+
+  def __call__(self, func):
+    if torch._jit_internal.is_scripting():
+      return func
+    return autocast_decorator(self, func)
+  
 class TorchAutocast:
     """TorchAutocast utility class.
     Allows you to enable and disable autocast. This is specially useful
@@ -19,7 +46,13 @@ class TorchAutocast:
         kwargs: Additional kwargs for torch.autocast
     """
     def __init__(self, enabled: bool, *args, **kwargs):
-        self.autocast = torch.autocast(*args, **kwargs) if enabled else None
+        #if torch.backends.mps.is_available():
+        #    try:
+        #        self.autocast = torch.autocast(enabled=enabled, device_type='mps')
+        #    except:
+        #        self.autocast = torch.autocast = totally_legit_autocast(enabled=enabled, *args, **kwargs)
+        #else:
+            self.autocast = torch.autocast(*args, **kwargs) if enabled else None
 
     def __enter__(self):
         if self.autocast is None:
